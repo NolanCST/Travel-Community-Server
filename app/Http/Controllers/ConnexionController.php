97d0c3895e0fb15682a48b820a47a89c05e7f4df
NolanCST <nolan.costa06@gmail.com>
2024-01-23 22:17:42 +0100

    inscription et connexion fonctionnelle

--- app/Http/Controllers/ConnexionController.php
+++ app/Http/Controllers/ConnexionController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use Hash;
 
 class ConnexionController extends Controller
 {
@@ -13,8 +17,7 @@ class ConnexionController extends Controller
             'pseudo' => 'required',
             'email' => 'required',
             'password' => 'required',
-            'country' => 'required',
-            'status' => 'required'
+            'country' => 'required'
         ]);
 
         $user = User::create([
@@ -24,12 +27,47 @@ class ConnexionController extends Controller
             'email' => $request->email,
             'password' => $request->password,
             'country' => $request->country,
-            'status' => $request->status
+            'status' => 0 // A voir comment je gère le status
         ]);
     }
 
-    public function login () {
+    public function login (Request $request) {
 
+        $credentials = Validator::make($request->all(), [
+            'pseudo' => 'required',
+            'password' => 'required',
+        ]);
+        if ($credentials->fails()) {
+            return Response()->json([
+                'validation_errors'=>$credentials->message(),
+            ]);
+        } else {
+            $user = User::where('pseudo', $request->pseudo)->first();
+
+            if(! $user || ! Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'status'=>401,
+                    'message'=>'Le pseudo ou le mot de passe est invalide',
+                ]);
+            } else {
+                $token = $user->createToken($user->email.'_token')->plainTextToken;
+
+                return response()->json([
+                    'status'=>200,
+                    'pseudo'=>$user->pseudo,
+                    'token'=>$token,
+                    'message'=>'Connexion réussie',
+                ]);
+            }
+        }
+
+        if (Auth::attempt(request()->except(['_token']))) {
+            $user = Auth::user();
+            // $success =  $user->createToken('MyApp')->plainTextToken;
+            return redirect()->route('admin.dashboard');
+        } else {
+            return Response(['email ou mot de passe incorect'], 401);
+        }
     }
 
     public function logout () {
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ConnexionController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [UserController::class], 'getUser')->name('user');
 });
 
+// Validation de l'inscription dans la BDD
 Route::post('/register', [ConnexionController::class, 'Register'])->name('register');
 
+// Connexion de l'utilisateur
+Route::post('/login', [ConnexionController::class, 'login'])->name('login');
+Route::post('/tokens/create', [AuthController::class, 'createToken']);
