9e0d71423a3a45082a9c988406e5c9ea4ceb14b7
NolanCST <nolan.costa06@gmail.com>
2024-01-26 22:51:05 +0100

    probleme avec sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ConnexionController;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +16,11 @@ use App\Http\Controllers\UserController;
 */
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [UserController::class], 'getUser')->name('user');
+    // Récupération de l'utilisateur
+    Route::get('/user', function(Request $request) {return $request->user();});
 });
+
+// Route::get('/user', function(Request $request) {return $request->user();});
 
 // Validation de l'inscription dans la BDD
 Route::post('/register', [ConnexionController::class, 'Register'])->name('register');
